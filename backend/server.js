422a858d164995004e04f88a6103c357e2b5b184
root <root@test.myguest.virtualbox.org>
2026-03-19 17:17:01 +0530

    jj

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,16 @@ app.get('/health', (req,res)=>{
     res.status(200).json({status:"ok"});
 });
 
-app.listen(5000, () => console.log('Backend running on port 5000'));
+
+
+app.listen(5000);
+
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`Running on ${PORT}`));
+
+
+
+//app.listen(5000, () => console.log('Backend running on port 5000'));
 
 //const PORT = process.env.PORT || 5000;
 
